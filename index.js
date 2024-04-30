4715a24e54a5c0b1f300e382c42a5c326684cc6d
innovationatwt <166100070+innovationatwt@users.noreply.github.com>
2024-04-30 13:32:50 +0530

    Updated index.js

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ import cors from "cors";
 import peopleRoutes from "./routes/people.js";
 
 const app = express();
-const PORT = process.env.PORT || 8000;
+const PORT = 8000;
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors());
